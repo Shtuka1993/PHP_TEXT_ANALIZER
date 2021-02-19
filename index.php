cd08197648f0dc0c504ba8168cefa02bdea86e97
yshtokaylo <yshtokaylo@artelogic.net>
2021-02-19 12:21:17 +0200

    Initialization

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    // Statistical information:
+    //  - Number of characters
+    //  - Number of words
+    //  - Number of sentences
+    //  - Frequency of characters
+    //  - Distribution of charactes as a percentage of total
+    //  - Average word length
+    //  - The average number of words in a sentence
+    //  - Top 10 most used words
+    //  - Top 10 longest words
+    //  - Top 10 shortest words
+    //  - Top 10 longest sentences
+    //  - Top 10 shortest sentences
+    //  - Number of palindrome words
+    //  - Is the whole text a palindrome?(Without whitespaces and punctuation marks.)
+    //  - The time it tooks to process the text in ms
+    //  - (Optional) Add any data that you think could be interesting
+
+    // Date and time when the report was generated
+
+    // The reversed text
+
+    // The calculation should work for UTF8 encoded text as well
