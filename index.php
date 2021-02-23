26b095a83ce77539b0037f80effdc1e82f08ed33
yshtokaylo <yshtokaylo@artelogic.net>
2021-02-23 23:39:12 +0200

    Added form

--- index.php
+++ index.php
@@ -320,10 +320,33 @@ function isPalindrome(string $text):bool {
     }
     
     // Text for data
-    $string = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+    //$string = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
     // Analize data
-    $text = new TextAnalizer($string);
+    //$text = new TextAnalizer($string);
 
     // Print analized data
-    echo $text;
+    //echo $text;
+?>
+
+<html>
+    <head>
+        <title>Text Analizer</title>
+    </head>
+    <body>
+        <form method="post" action="">
+            <textarea required name="text"></textarea><br>
+            <input type="submit" value="SEND"><br>
+        </form>
+            <?php
+                if(isset($_POST['text'])) {
+                	echo('<p><pre>');
+                	$text = trim($_POST['text']);
+                	$text = stripcslashes($text);
+                	$text = htmlspecialchars($text);
+                    echo new TextAnalizer($text);
+                    echo('</pre><p>');
+                }
+            ?>
+    </body>
+</html>
