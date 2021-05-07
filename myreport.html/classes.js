89152eb37728e4fdfe225ea500d84ffd338571dd
yshtokaylo <yshtokaylo@artelogic.net>
2021-05-07 20:39:42 +0300

    Added phpmetrics

---
+++ myreport.html/classes.js
@@ -0,0 +1,145 @@
+var classes = [
+    {
+        "name": "app\\TextAnalizer",
+        "interface": false,
+        "abstract": false,
+        "final": false,
+        "methods": [
+            {
+                "name": "__construct",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "__toString",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "calculateLength",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "generateReversedText",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "generateListOfWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "generateListOfSentences",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setWordsByUsage",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setWordsByLength",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setSentancesByLength",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setAvaregeLengthOfWord",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setAvaregeNumberOfWordsInSentances",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setDistributionOfCharacters",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "setNumberOfPalindromes",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            }
+        ],
+        "nbMethodsIncludingGettersSetters": 13,
+        "nbMethods": 13,
+        "nbMethodsPrivate": 7,
+        "nbMethodsPublic": 6,
+        "nbMethodsGetter": 0,
+        "nbMethodsSetters": 0,
+        "wmc": 26,
+        "ccn": 14,
+        "ccnMethodMax": 4,
+        "externals": [],
+        "parents": [],
+        "lcom": 5,
+        "length": 443,
+        "vocabulary": 86,
+        "volume": 2846.84,
+        "difficulty": 24.22,
+        "effort": 68948.01,
+        "level": 0.04,
+        "bugs": 0.95,
+        "time": 3830,
+        "intelligentContent": 117.54,
+        "number_operators": 171,
+        "number_operands": 272,
+        "number_operators_unique": 13,
+        "number_operands_unique": 73,
+        "cloc": 92,
+        "loc": 270,
+        "lloc": 178,
+        "mi": 64.14,
+        "mIwoC": 24.84,
+        "commentWeight": 39.3,
+        "kanDefect": 1.83,
+        "relativeStructuralComplexity": 64,
+        "relativeDataComplexity": 0.88,
+        "relativeSystemComplexity": 64.88,
+        "totalStructuralComplexity": 832,
+        "totalDataComplexity": 11.44,
+        "totalSystemComplexity": 843.44,
+        "package": "app\\",
+        "pageRank": 1,
+        "afferentCoupling": 0,
+        "efferentCoupling": 0,
+        "instability": 0,
+        "violations": {}
+    }
+]
